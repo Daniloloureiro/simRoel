1d3023def9751f2f0e8821c776e808df192f7955
danilo-eclipse <daniloloureiro.dl@gmail.com>
2024-04-25 22:32:32 -0300

    mudando name

--- app.js
+++
@@ -1,5 +0,0 @@
-function mostrar(event) {
-    event.preventDefault();
-    var nome = document.getElementById('nome').value;
-    document.getElementById('texto').innerHTML = "Olá, " + nome + "!";
-}
